769abb5cab4fefd4607d01407b4fa9e03a464a96
ckieverentineas <ckieverentineas@yandex.ru>
2022-06-09 13:12:16 +1000

    added toogle test

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,9 +1,15 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 const Test: NextPage = () => {
+    const [methodEducation, setMethodEducation] = useState('');
+    const handleChange=async (e: { target: { value: any } })=>{
+        await setMethodEducation(e.target.value);
+        await console.log(methodEducation)
+    }
     return (
         <div className={styles.container}>
         <Head>
@@ -16,14 +22,22 @@ const Test: NextPage = () => {
             <h1 className={styles.title}>
             Выберите форму обучения
             </h1>
-
             <div className={styles.grid}>
-            <a className={styles.card}>
-                очное
-            </a>
-            <a className={styles.card}>
-                заочное
-            </a>
+            <form>
+                <div>
+                <a className={styles.card}>
+                <input type="radio" value="ОЧНОЕ" id="method_education"
+                onChange={handleChange} name="method_education" />
+                <label>ОЧНОЕ</label>
+                </a>
+                <a className={styles.card}>
+                <input type="radio" value="ЗАОЧНОЕ" id="method_education"
+                onChange={handleChange} name="method_education"/>
+                <label>ЗАОЧНОЕ</label>
+                </a>
+                </div>
+                <div><p>Вы выбрали способ обучения --{'>'} {methodEducation}</p>  </div>
+            </form>
             </div>
         </main>
 
