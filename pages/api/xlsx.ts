b08ff16f3be4decff35ac5799cd2728daf58c38b
ckieverentineas <ckieverentineas@yandex.ru>
2022-06-16 05:39:30 +1000

    creation data for excel

--- pages/api/xlsx.ts
+++ pages/api/xlsx.ts
@@ -3,54 +3,42 @@ import { promises as fs } from 'fs';
 import {Document, Drawing} from 'docx'
 import createReport from 'docx-templates';
 import { Children } from 'react';
-import xlsx from 'json-as-xlsx';
+import xlsx from 'xlsx';
 
 const prisma = new PrismaClient();
 
 export default async function handler(req: any, res: any) {
-    if (req.method === 'GET') {
+    if (req.method === 'POST') {
         let { specialization_first } = req.body
-        specialization_first = "КС"
         console.log(req.body)
         const data = await prisma.passport.findMany({
             where: {
                 specialization_first
-            }}
-        )
-        let i = await Object.keys(data).length;
-        console.log(i)
-        let sta = ''
-        for(let t=0; t<i; t++) {
-            sta += data[t]+', '
-        }
-        console.log(sta)
-        const testing: any[] = [
-            {
-                sheet: "Adults",
-                columns: [
-                    { label: "№ п/п", value: (rows) => i++ },
-                    { label: "ФИО абитуриента", value: (rows: { firstname: any; name: any; lastname: any; }) => rows.firstname + " " + rows.name + " " + rows.lastname},
-                    { label: "ср. балл", value: (rows: { tree: string; four: string; five: string; }) => ((parseInt(rows.tree)*3+parseInt(rows.four)*4+parseInt(rows.five)*5)/(parseInt(rows.tree)+parseInt(rows.four)+parseInt(rows.five))).toFixed(2)},
-                    { label: "копия/оригинал", value: 'education_complete_type'}
-                ],
-                content: [
-                    sta
-                ],
-            }
-        ]
-        const settings: any = {
-            writeOptions: {
-                type: "buffer",
-                bookType: "xlsx",
             },
+            select: {
+                id: true,
+                firstname: true,
+                name: true,
+                lastname: true,
+                tree:true,
+                four: true,
+                five: true,
+                education_complete_type: true
+            }
         }
+            
+        )
 
-        const buffer = xlsx(testing, settings)
-        res.writeHead(200, {
-                "Content-Type": "application/octet-stream",
-                "Content-disposition": "attachment; filename=MySheet.xlsx",
-        })
-        res.end(buffer)
+        const WorkSheet = xlsx.utils.json_to_sheet(data)
+        const WorkBook = xlsx.utils.book_new()
+        xlsx.utils.book_append_sheet(WorkBook, WorkSheet, "Dates");
+
+        /* fix headers */
+        xlsx.utils.sheet_add_aoa(WorkSheet, [["№ п/п", "Фамилия", "Имя", "Отчество", "тройки", "четверки", "пятерки", "копия/оригинал"]], { origin: "A1" });
+
+        /* create an XLSX file and try to save to Presidents.xlsx */
+        const sela = xlsx.writeFile(WorkBook, `./public/tables/${specialization_first}.xlsx`);
+        await res.status(200).json({link: `./public/tables/${specialization_first}.xlsx`})
     }
     prisma.$disconnect()
 }
